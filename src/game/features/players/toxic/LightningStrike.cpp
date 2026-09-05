6d4f595088ddf1990572f58c8355a5bc9e4cd7c6
Deadlineem <jownsall@gmail.com>
2026-09-05 02:07:07 -0400

    Test Session Switching using Globals

--- src/game/features/players/toxic/LightningStrike.cpp
+++ src/game/features/players/toxic/LightningStrike.cpp
@@ -1,9 +1,10 @@
+#include "core/commands/LoopedCommand.hpp"
 #include "game/commands/PlayerCommand.hpp"
-#include "game/features/Features.hpp"
 #include "game/pointers/Pointers.hpp"
 #include "game/rdr/Enums.hpp"
 #include "game/rdr/Natives.hpp"
-
+#include "game/backend/ScriptMgr.hpp"
+#include "game/backend/Self.hpp"
 
 namespace YimMenu::Features
 {
@@ -19,4 +20,63 @@ namespace YimMenu::Features
 	};
 
 	static LightningStrike _LightningStrike{"lightning", "Lightning Strike", "Strikes the player with lightning"};
+
+	BoolCommand _LightningLoopActive{"lightningactive", "Lightning Loop Enabled", "Turn off to stop lightning", false};
+
+	class LightningStrikeLoop : public PlayerCommand
+	{
+		using PlayerCommand::PlayerCommand;
+
+		virtual void OnCall(Player player) override
+		{
+			if (!player.IsValid())
+				return;
+
+			_LightningLoopActive.SetState(true);
+
+			for (int i = 0; i < 250; i++)
+			{
+				if (!_LightningLoopActive.GetState())
+				{
+					_LightningLoopActive.SetState(false);
+					return;
+				}
+
+				while (PED::IS_PED_DEAD_OR_DYING(player.GetPed().GetHandle(), true)
+				    || PLAYER::IS_PLAYER_DEAD(player.GetPed().GetHandle()))
+				{
+					if (!_LightningLoopActive.GetState() || !player.IsValid())
+					{
+						_LightningLoopActive.SetState(false);
+						return;
+					}
+
+					ScriptMgr::Yield(500ms);
+
+					if (!player.IsValid())
+					{
+						_LightningLoopActive.SetState(false);
+						return;
+					}
+				}
+
+				if (!_LightningLoopActive.GetState())
+				{
+					_LightningLoopActive.SetState(false);
+					return;
+				}
+
+				if (!PLAYER::IS_PLAYER_DEAD(player.GetPed().GetHandle()) || !PED::IS_PED_DEAD_OR_DYING(player.GetPed().GetHandle(), true))
+				{
+					auto playerCoords = player.GetPed().GetPosition();
+					
+					FIRE::ADD_EXPLOSION(playerCoords.x, playerCoords.y, playerCoords.z, 33, 5.0f, true, false, 5.0f);
+					ScriptMgr::Yield(150ms);
+				}
+			}
+			_LightningLoopActive.SetState(false);
+		}
+	};
+
+	static LightningStrikeLoop _LightningStrikeLoop{"lightningloop", "Lightning Loop", "Loops 2500 lightning strikes on the selected player ONLY while they are alive"};
 }
